410725eac25ef8daba4084eb6938186430b16dc8
Evgeniy Lapin <winstyson@gmail.com>
2021-02-01 23:08:51 +0300

    CompanyStaff release

---
+++ task-3/3-CompanyStaff/CompanyStaff/CompanyStaff/src/com/senla/lapin/task2/Person.java
@@ -0,0 +1,17 @@
+package com.senla.lapin.task2;
+
+public class Person {
+	
+	private String name;
+	
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+}
